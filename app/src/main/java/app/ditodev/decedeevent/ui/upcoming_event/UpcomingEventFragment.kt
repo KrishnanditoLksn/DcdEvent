67a3d4751ee11d4acaab2b5f4c88c7f3d780be07
Gorthdzx <krislaksana123@gmail.com>
2024-10-01 18:47:44 +0700

    feat : linear layout manager and view-models observer

--- app/src/main/java/app/ditodev/decedeevent/ui/upcoming_event/UpcomingEventFragment.kt
+++ app/src/main/java/app/ditodev/decedeevent/ui/upcoming_event/UpcomingEventFragment.kt
@@ -1,60 +1,52 @@
 package app.ditodev.decedeevent.ui.upcoming_event
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import app.ditodev.decedeevent.R
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import app.ditodev.decedeevent.data.api.response.ListEventsItem
+import app.ditodev.decedeevent.databinding.FragmentUpcomingEventBinding
+import app.ditodev.decedeevent.ui.adapter.UpcomingEventAdapter
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [UpcomingEventFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class UpcomingEventFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    private lateinit var binding: FragmentUpcomingEventBinding
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val layoutManager = LinearLayoutManager(requireActivity())
+        binding.rvEvent.layoutManager = layoutManager
+        val itemDecoration = DividerItemDecoration(requireActivity(), layoutManager.orientation)
+        binding.rvEvent.addItemDecoration(itemDecoration)
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
+        val viewModel = ViewModelProvider(this)[UpcomingEventViewModel::class.java]
+        viewModel.listEvent.observe(
+            viewLifecycleOwner
+        ) {
+            setupRecyclerView(it)
+        }
+
+        viewModel.isLoading.observe(
+            viewLifecycleOwner
+        ) {
+            binding.pbLoading.visibility = if (it) View.VISIBLE else View.GONE
         }
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_upcoming_event, container, false)
+        binding = FragmentUpcomingEventBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment UpcomingEventFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            UpcomingEventFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    private fun setupRecyclerView(listEventsItem: List<ListEventsItem>) {
+        val adapter = UpcomingEventAdapter()
+        adapter.submitList(listEventsItem)
+        binding.rvEvent.adapter = adapter
     }
 }
