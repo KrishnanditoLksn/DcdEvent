69c92827008646584ea29bffd3ee34b43ad0cbbb
Gorthdzx <krislaksana123@gmail.com>
2024-10-01 11:22:53 +0700

    feat: Api configuration using Retrofit

--- app/src/main/java/app/ditodev/decedeevent/data/api/config/ApiConfig.kt
+++ app/src/main/java/app/ditodev/decedeevent/data/api/config/ApiConfig.kt
@@ -1,4 +1,28 @@
 package app.ditodev.decedeevent.data.api.config
 
+import app.ditodev.decedeevent.data.api.service.ApiService
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
 class ApiConfig {
+    companion object {
+        fun getApiService(): ApiService {
+            val loggingInterceptor =
+                HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)
+
+            val client = OkHttpClient.Builder()
+                .addInterceptor(loggingInterceptor)
+                .build()
+
+            val retrofit = Retrofit.Builder()
+                .baseUrl("https://event-api.dicoding.dev")
+                .addConverterFactory(GsonConverterFactory.create())
+                .client(client)
+                .build()
+
+            return retrofit.create(ApiService::class.java)
+        }
+    }
 }
