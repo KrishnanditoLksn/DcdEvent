050433d2532bb1850235c23eeb3c2c32cea1d45e
Gorthdzx <krislaksana123@gmail.com>
2024-10-03 09:42:50 +0700

    feat : fragment and viewmodel for finished events

--- app/src/main/java/app/ditodev/decedeevent/ui/event/finished_event/FinishedEventFragment.kt
+++ app/src/main/java/app/ditodev/decedeevent/ui/event/finished_event/FinishedEventFragment.kt
@@ -6,20 +6,49 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import app.ditodev.decedeevent.data.api.response.ListEventsItem
 import app.ditodev.decedeevent.databinding.FragmentFinishedEventBinding
+import app.ditodev.decedeevent.ui.adapter.UpcomingEventAdapter
 
 class FinishedEventFragment : Fragment() {
 
     private lateinit var binding: FragmentFinishedEventBinding
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val linearLayout = LinearLayoutManager(requireActivity())
+        binding.rvFinishedEvent.layoutManager = linearLayout
+        val itemDecoration = DividerItemDecoration(requireActivity(), linearLayout.orientation)
+        binding.rvFinishedEvent.addItemDecoration(itemDecoration)
+
+        val viewModel = ViewModelProvider(this)[FinishedEventViewModel::class.java]
+
+        viewModel.finishedListEvents.observe(
+            viewLifecycleOwner
+        ) {
+            setFinishedEvents(it)
+        }
+
+        viewModel.isLoading.observe(viewLifecycleOwner) {
+            binding.pbLoading.visibility = if (it) View.VISIBLE else View.GONE
+        }
+    }
+
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val notificationsViewModel =
-            ViewModelProvider(this)[NotificationsViewModel::class.java]
         binding = FragmentFinishedEventBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    private fun setFinishedEvents(listEvents: List<ListEventsItem>) {
+        val adapter = UpcomingEventAdapter()
+        adapter.submitList(listEvents)
+        binding.rvFinishedEvent.adapter = adapter
     }
 }
---
+++ app/src/main/java/app/ditodev/decedeevent/ui/event/finished_event/FinishedEventViewModel.kt
@@ -0,0 +1,55 @@
+package app.ditodev.decedeevent.ui.event.finished_event
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import app.ditodev.decedeevent.data.api.config.ApiConfig
+import app.ditodev.decedeevent.data.api.response.EventResponse
+import app.ditodev.decedeevent.data.api.response.ListEventsItem
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+
+class FinishedEventViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is notifications Fragment"
+    }
+    val text: LiveData<String> = _text
+
+    private var _finishedListEvents = MutableLiveData<List<ListEventsItem>>()
+    val finishedListEvents: LiveData<List<ListEventsItem>> = _finishedListEvents
+
+    private var _isLoading = MutableLiveData<Boolean>()
+    val isLoading: LiveData<Boolean> = _isLoading
+
+
+    init {
+        getFinishedEventsList()
+    }
+
+    private fun getFinishedEventsList() {
+        _isLoading.value = true
+        val client = ApiConfig.getApiService().getListEvents("0")
+
+        client.enqueue(
+            object : Callback<EventResponse> {
+                override fun onResponse(
+                    call: Call<EventResponse>,
+                    response: Response<EventResponse>
+                ) {
+                    _isLoading.value = false
+                    val responseBody = response.body()
+                    if (response.isSuccessful) {
+                        _finishedListEvents.value = responseBody?.listEvents
+                    }
+                }
+
+                override fun onFailure(call: Call<EventResponse>, t: Throwable) {
+                    _isLoading.value = false
+                }
+
+            }
+        )
+    }
+}
