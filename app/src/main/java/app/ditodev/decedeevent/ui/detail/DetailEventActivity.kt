ed4525e70bad4f87e671960f6e1b80aec3ebb350
Gorthdzx <krislaksana123@gmail.com>
2024-10-03 07:49:47 +0700

    feat : activity to display detail of event

--- app/src/main/java/app/ditodev/decedeevent/ui/detail/DetailEventActivity.kt
+++ app/src/main/java/app/ditodev/decedeevent/ui/detail/DetailEventActivity.kt
@@ -1,8 +1,11 @@
 package app.ditodev.decedeevent.ui.detail
 
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.text.HtmlCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.lifecycle.ViewModelProvider
@@ -24,25 +27,44 @@ class DetailEventActivity : AppCompatActivity() {
         }
 
         val viewModel = ViewModelProvider(this)[DetailEventViewModel::class.java]
-        val eventId = intent.getIntExtra(Util.EXTRA_ID , 0)
+        val eventId = intent.getIntExtra(Util.EXTRA_ID, 0)
         Log.d("IniHalamanDetail", "onCreate: $eventId")
         viewModel.fetchDetailEvents(eventId)
 
-        viewModel.listEvent.observe(this) { event ->
+        viewModel.detailEvent.observe(this) { event ->
             if (event != null) {
                 Glide.with(this)
-                    .load(event)
+                    .load(event.event?.imageLogo)
                     .into(binding.ivDetailImage)
-                Log.d("IniRespondariApi", "onCreate: ${event.name}")
-                binding.tvDetailName.text = event.name
-                binding.tvOwnerName.text = event.ownerName
+                Log.d("IniRespondariApi", "onCreate: ${event.event?.name}")
+                binding.tvDetailName.text = event.event?.name
+                binding.tvOwnerName.text = event.event?.ownerName
+                binding.tvBeginTime.text = event.event?.beginTime
+                binding.tvQuota.text = event.event?.quota.toString()
+
+                binding.tvDescription.text = event.event?.description?.let {
+                    HtmlCompat.fromHtml(
+                        it,
+                        HtmlCompat.FROM_HTML_MODE_LEGACY
+                    )
+                }
+
+                binding.btnShare.setOnClickListener {
+                    val share = Intent.createChooser(Intent().apply {
+                        action = Intent.ACTION_SEND
+                        putExtra(Intent.EXTRA_TEXT, event.event?.link)
+                        type = "text/plain"
+                    }, null)
+
+                    startActivity(share)
+                }
             }
         }
 
-//        viewModel.isLoading.observe(
-//            this
-//        ) {
-//            binding.pbLoad.visibility = if (it) View.VISIBLE else View.GONE
-//        }
+        viewModel.isLoading.observe(
+            this
+        ) {
+            binding.pbLoad.visibility = if (it) View.VISIBLE else View.GONE
+        }
     }
 }
