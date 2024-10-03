e4d54f146cf466635a9dd61b9b576770ee5036f2
Gorthdzx <krislaksana123@gmail.com>
2024-10-03 09:44:38 +0700

    fix : get method using id params

--- app/src/main/java/app/ditodev/decedeevent/data/api/service/ApiService.kt
+++ app/src/main/java/app/ditodev/decedeevent/data/api/service/ApiService.kt
@@ -1,9 +1,10 @@
 package app.ditodev.decedeevent.data.api.service
 
+import app.ditodev.decedeevent.data.api.response.DetailEventResponse
 import app.ditodev.decedeevent.data.api.response.EventResponse
 import retrofit2.Call
 import retrofit2.http.GET
-import retrofit2.http.POST
+import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface ApiService {
@@ -11,5 +12,6 @@ interface ApiService {
     fun getListEvents(@Query("active") page: String): Call<EventResponse>
 
     @GET("/events/{id}")
-    fun getEventDetail(@Query("id") id: Int): Call<EventResponse>
+    fun getEventDetail(@Path("id") id: Int): Call<DetailEventResponse>
+
 }
