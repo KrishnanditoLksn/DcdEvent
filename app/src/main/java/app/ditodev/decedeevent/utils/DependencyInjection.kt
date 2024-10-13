2784c8adf9908c1e9651bc9230a60a8986b4f1e1
Gorthdzx <krislaksana123@gmail.com>
2024-10-13 16:16:00 +0700

    feat : DependencyInjection

---
+++ app/src/main/java/app/ditodev/decedeevent/utils/DependencyInjection.kt
@@ -0,0 +1,23 @@
+package app.ditodev.decedeevent.utils
+
+import android.content.Context
+import app.ditodev.decedeevent.data.local.EventDatabase
+import app.ditodev.decedeevent.data.local.repository.DetailEventRepository
+import app.ditodev.decedeevent.data.local.repository.FavoriteEventRepository
+import app.ditodev.decedeevent.data.remote.api.config.ApiConfig
+
+object DependencyInjection {
+    fun provideRepository(context: Context): DetailEventRepository {
+        val apiService = ApiConfig.getApiService()
+        val database = EventDatabase.getInstance(context)
+        val dao = database.eventDao()
+        return DetailEventRepository.getInstance(apiService, dao)
+    }
+
+    fun provideFavoriteRepository(context: Context): FavoriteEventRepository {
+        val apiService = ApiConfig.getApiService()
+        val database = EventDatabase.getInstance(context)
+        val dao = database.eventDao()
+        return FavoriteEventRepository.getInstance(apiService, dao)
+    }
+}
