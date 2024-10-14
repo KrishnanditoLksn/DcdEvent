9561dd20daa501270eb28faa8dc1732a9f8095d5
Gorthdzx <krislaksana123@gmail.com>
2024-10-14 10:52:50 +0700

    fix : theme  settings when app launch again always consistent

--- app/src/main/java/app/ditodev/decedeevent/MainActivity.kt
+++ app/src/main/java/app/ditodev/decedeevent/MainActivity.kt
@@ -2,16 +2,23 @@ package app.ditodev.decedeevent
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import app.ditodev.decedeevent.databinding.ActivityMainBinding
+import app.ditodev.decedeevent.ui.settings.SettingsDataStore
+import app.ditodev.decedeevent.ui.settings.SettingsFactory
+import app.ditodev.decedeevent.ui.settings.SettingsViewModel
+import app.ditodev.decedeevent.ui.settings.dataStore
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var vm : SettingsViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -31,5 +38,17 @@ class MainActivity : AppCompatActivity() {
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+
+
+        val pref = SettingsDataStore.getInstance(this.dataStore)
+        val factory = SettingsFactory(pref)
+        vm = ViewModelProvider(this, factory)[SettingsViewModel::class.java]
+        vm.getThemeSettings().observe(this){
+            if(it){
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+            }else{
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+            }
+        }
     }
 }
