2634d90b6169c8ad14f406f21772683bd32936eb
Gorthdzx <krislaksana123@gmail.com>
2024-10-20 14:04:30 +0700

    style : login , register , and welcome screen activity

---
+++ app/src/main/java/app/ditodev/decedeevent/ui/register/RegisterScreenActivity.kt
@@ -0,0 +1,21 @@
+package app.ditodev.decedeevent.ui.register
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import app.ditodev.decedeevent.R
+
+class RegisterScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_register_screen)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.register)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
---
+++ app/src/main/java/app/ditodev/decedeevent/ui/welcome/WelcomeScreenActivity.kt
@@ -0,0 +1,21 @@
+package app.ditodev.decedeevent.ui.welcome
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import app.ditodev.decedeevent.R
+
+class WelcomeScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_welcome_screen)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.background)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
