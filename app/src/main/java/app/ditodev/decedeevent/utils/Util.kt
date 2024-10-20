928233759fe08ed4471ca502b970e1db7db18eb4
Gorthdzx <krislaksana123@gmail.com>
2024-10-20 13:18:03 +0700

    fix : diff util callback id

--- app/src/main/java/app/ditodev/decedeevent/utils/Util.kt
+++ app/src/main/java/app/ditodev/decedeevent/utils/Util.kt
@@ -1,17 +1,17 @@
 package app.ditodev.decedeevent.utils
 
 import androidx.recyclerview.widget.DiffUtil
-import app.ditodev.decedeevent.data.api.response.ListEventsItem
+import app.ditodev.decedeevent.data.remote.api.response.ListEventsItem
 
 object Util {
     const val EXTRA_ID = "extra_id"
     val DIFF_CALLBACK = object : DiffUtil.ItemCallback<ListEventsItem>() {
         override fun areItemsTheSame(oldItem: ListEventsItem, newItem: ListEventsItem): Boolean {
-            return oldItem == newItem
+            return oldItem.id == newItem.id
         }
 
         override fun areContentsTheSame(oldItem: ListEventsItem, newItem: ListEventsItem): Boolean {
-            return oldItem == newItem
+            return oldItem.id == newItem.id
         }
     }
 }
